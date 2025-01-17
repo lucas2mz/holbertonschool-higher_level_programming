95809c02e1eaac3382b710722f4d4f7ef9548f01
lucas2mz <lucasandrada2606@gmail.com>
2025-01-17 14:54:40 -0300

    Task 11

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,14 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    if b == 0:
-        return 1
-
-    result = 1
-    negative = b < 0
-    b = abs(b)
-
-    for i in range(0, b):
-        result = (a * a)
-        if negative:
-            return 1 / result
-    return result
+    return (a ** b)
