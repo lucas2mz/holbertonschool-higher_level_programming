fea6895d39e7adef72f96d6b084fe5116919f936
lucas2mz <lucasandrada2606@gmail.com>
2025-01-18 15:54:50 -0300

    Task 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+
+def main():
+    arguments = sys.argv[1:]
+    argc = len(arguments)
+
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+        print("1: {}".format(argc[0]))
+    else:
+        print("{} arguments:".format(argc))
+        for i, arg in enumerate(arguments, start=1):
+            print("{}: {}".format(i, arg))
+if __name__ == "__main__":
+    main()
