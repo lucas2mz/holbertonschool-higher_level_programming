bd948419b82f0ada66c17b22e7eee9e55d710345
lucas2mz <lucasandrada2606@gmail.com>
2025-01-19 16:17:10 -0300

    Task 3

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+
+def main():
+    arguments = sys.argv[1:]
+
+    result = sum(int(arg) for arg in arguments)
+
+    print(result)
+
+if __name__ == "__main__":
+    main()
