12cb4fbb29f50de02d41f072429150556704e4a8
lucas2mz <lucasandrada2606@gmail.com>
2025-01-24 16:26:00 -0300

    Task 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tuple_sentence = (len(sentence), sentence[0])
+    if sentence == "":
+        tuple_sentence = (0, None)
+    else:
+        tuple_sentence = (len(sentence), sentence[0])
 
-    return tuple_sentence 
+    return tuple_sentence
