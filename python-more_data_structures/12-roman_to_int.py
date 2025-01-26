b1d47de6a1383a991f83fcaf0547c2cb30e78d88
lucas2mz <lucasandrada2606@gmail.com>
2025-01-26 20:48:06 -0300

    Task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,10 +4,10 @@ def roman_to_int(roman_string):
         return 0
 
     num_rom = {
-    'I': 1, 'V': 5, 'X': 10, 
-    'L': 50, 'C': 100, 
-    'D': 500, 'M': 1000
-}
+        'I': 1, 'V': 5, 'X': 10, 
+        'L': 50, 'C': 100, 
+        'D': 500, 'M': 1000
+    }
     total = 0
     prev = 0
 
