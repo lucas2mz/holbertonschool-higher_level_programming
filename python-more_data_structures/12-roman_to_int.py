535df9b07fa1b394b7c2e3f8d3611b13c54202b1
lucas2mz <lucasandrada2606@gmail.com>
2025-01-26 20:46:46 -0300

    Task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is not str or None:
+    if not isinstance(roman_string, str) or not roman_string:
         return 0
 
-    num_rom = dict(I = "1", V = "5", X = "10", L = "50", C = "100", D = "500", M = "1000")
+    num_rom = {
+    'I': 1, 'V': 5, 'X': 10, 
+    'L': 50, 'C': 100, 
+    'D': 500, 'M': 1000
+}
+    total = 0
+    prev = 0
 
-    for key, value in num_rom.items():
-        if roman_string == num_rom[key]:
-            return map(int, value)
+    for char in reversed(roman_string):
+        if char not in num_rom:
+            return 0
+        current = num_rom[char]
+        if current < prev:
+            total -= current
+        else:
+            total += current
+            prev = current
+    return total
