b14c353796710d9c73e56a2cf055f21204f5ea1c
lucas2mz <lucasandrada2606@gmail.com>
2025-01-26 23:12:10 -0300

    Task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
             count += 1
         except (ValueError, TypeError):
             continue
-        except IndexError:
-            break
     print()
+    if x > len(my_list):
+        raise IndexError("list index out of range")
     return count
