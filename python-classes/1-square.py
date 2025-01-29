971dc9efa982c80d68f258740a8f023451998a32
lucas2mz <lucasandrada2606@gmail.com>
2025-01-29 12:47:35 -0300

    Task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+This module defines a class Square.
+The Square class represents a geometric square object.
+'''
+
+
+class Square:
+    '''
+    This class defines a square.
+    It currently has no attributes or methods.
+    '''
+    def __init__(self, size):
+        self.__size = size 
