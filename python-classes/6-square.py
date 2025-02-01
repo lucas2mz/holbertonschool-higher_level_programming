1cc208b7a0890488ac024243d5bd9518c7df197d
lucas2mz <lucasandrada2606@gmail.com>
2025-02-01 11:17:07 -0300

    Task 6

---
+++ python-classes/6-square.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+'''
+This module defines a class Square.
+The Square class represents a geometric square object.
+'''
+
+
+class Square:
+    '''
+    This class defines a square.
+    It currently has no attributes or methods.
+    '''
+    def __init__(self, size=0, position=(0, 0)):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        return (self.size * self.size)
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        for i in range(self.size):
+            print('#' * self.size)
+
+        for _ in range(self.position[1]):
+            print()
+
+        for _ in range(self.size):
+            print("_" * self.position[0] + "#" * self.size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self._position = value
