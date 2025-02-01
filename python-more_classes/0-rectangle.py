9b4725d351ebdb239a6d92b68051d59c3f4dfd0f
lucas2mz <lucasandrada2606@gmail.com>
2025-02-01 11:40:32 -0300

    Task 0

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+This module defines a class Rectangle.
+The Rectangle class represents a geometric rectangle object.
+'''
+
+
+class Rectangle:
+    '''
+    This class defines a rectangle.
+    It currently has no attributes or methods.
+    '''
+    pass
