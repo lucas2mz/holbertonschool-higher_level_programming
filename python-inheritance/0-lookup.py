03f2e3f028960f7406531c8f2d4e41645a216a7a
lucas2mz <lucasandrada2606@gmail.com>
2025-02-05 12:13:24 -0300

    Task 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -6,4 +6,8 @@ The Square class represents a geometric square object.
 
 
 def lookup(obj):
+    '''
+    This method give us list of 
+    available attributes and methods
+    '''
     return dir(obj)
