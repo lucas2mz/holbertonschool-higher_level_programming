edc511052e50e8c637d39378c078e0c64761339f
lucas2mz <lucasandrada2606@gmail.com>
2025-02-08 14:37:37 -0300

    Task 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -4,13 +4,16 @@ module
 '''
 
 
-BaseGeometry = __import__('9-rectangle').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Square(BaseGeometry):
+class Square(Rectangle):
+    '''
+    class
+    '''
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
-        return size * size
+        return self.__size * self.__size
