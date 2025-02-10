06e8b9a544543e499627c8b6bf2f486f7cc93d8d
lucas2mz <lucasandrada2606@gmail.com>
2025-02-09 23:19:01 -0300

    Task 5

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -13,7 +13,7 @@ class Shape(ABC):
 
 class Circle(Shape):
     def __init__(self, radius):
-        self.__radius = radius
+        self.__radius = abs(radius)
 
     def area(self):
         return math.pi * (self.__radius ** 2)
