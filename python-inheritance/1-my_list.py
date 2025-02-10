a98c8a9c48be527a3909d121ba4659190250725e
lucas2mz <lucasandrada2606@gmail.com>
2025-02-09 23:52:36 -0300

    Task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -8,7 +8,6 @@ the class MyList print a sorted list
 class MyList(list):
     """
 This class inherits from the built-in Python list class.
-It adds a method to print the list in ascending order without modifying the original list.
     """
     def print_sorted(self):
         print(sorted(self))
