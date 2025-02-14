938ef833fadc811ab867f6b50d2eaec12ba16aa1
lucas2mz <lucasandrada2606@gmail.com>
2025-02-14 15:26:26 -0300

    Task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,8 +5,8 @@ modulo
 
 
 import json
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+from ('5-save_to_json_file') import save_to_json_file
+from ('6-load_from_json_file') import load_from_json_file
 
 
 filename = "add_item.json"
