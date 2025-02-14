14c55cceedf6473dbe0de5a120ccf0938fe44354
lucas2mz <lucasandrada2606@gmail.com>
2025-02-14 13:20:51 -0300

    Task 6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+modulo
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    load_from_json_file
+    """
+    with open(filename, 'r') as file:
+        return json.loads(file)
