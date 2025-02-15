9b3e0c1c89bf8a58664e99e55954a3aaceeb1bf0
lucas2mz <lucasandrada2606@gmail.com>
2025-02-14 22:13:09 -0300

    Task 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+modulo
+"""
+
+
+class Student:
+    """
+    class student
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        return {key: V for key, V in self.__dict__.items() if key in attrs}
+
+    def reload_from_json(self, json):
+        
