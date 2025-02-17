e2dee6623233fa52bfc8c68281a5081d54748639
lucas2mz <lucasandrada2606@gmail.com>
2025-02-16 21:02:06 -0300

    Task 0

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+modulo
+"""
+
+
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    with open(filename, 'w') as file:
+        json.dump(data, file)
+
+def load_and_deserialize(filename):
+    with open(filename, 'r') as file:
+        return json.load(file)
