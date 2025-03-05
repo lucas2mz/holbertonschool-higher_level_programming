6dec22faa40d0e340b86e45b313a5a27fc3c8d1b
lucas2mz <lucasandrada2606@gmail.com>
2025-03-05 10:04:40 -0300

    Task 10

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,5 +1,2 @@
 -- LIST ALL RECORDES OF A TABLE
-SELECT (score, name)
-FROM second_table
-WHERE score >= 10
-ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
