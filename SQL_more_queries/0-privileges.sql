4a14e351b7c5a7f272d1e3f1f63b23a3b72d1ec4
lucas2mz <lucasandrada2606@gmail.com>
2025-03-10 12:22:57 -0300

    task 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- List all privileges of a MySQL users
-SHOW GRANTS FOR 'user_0d_1';
-SHOW GRANTS FOR 'user_0d_2';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
