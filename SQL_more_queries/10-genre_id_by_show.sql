b50a1f00483c4e98dd8927d6cfd443101a336e3c
lucas2mz <lucasandrada2606@gmail.com>
2025-03-10 14:39:31 -0300

    task 11

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- Lists all shows contained in hbtn_0d_tvshows
 SELECT tv_shows.title, tv_show_genres.genre_id 
-FROM tv_shows, tv_show_genres
+FROM tv_shows, tv_show_genres 
 WHERE tv_shows.id = tv_show_genres.show_id 
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
