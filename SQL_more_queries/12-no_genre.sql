ecbba6764102fc8079ace5e30f98058ee0beb073
lucas2mz <lucasandrada2606@gmail.com>
2025-03-10 14:48:28 -0300

    task 12

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,6 @@
 --  Lists all shows contained in the database hbtn_0d_tvshows without a genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id 
 FROM tv_shows
-INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC; 
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
