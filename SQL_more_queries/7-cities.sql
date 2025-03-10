dfe766f63fdb59bb05d636b16fbfc622e6d64eea
lucas2mz <lucasandrada2606@gmail.com>
2025-03-10 13:16:52 -0300

    task 7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -5,6 +5,6 @@ USE hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS cities (
     id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    state_id INT NOT NULL FOREIGN KEY (state_id) REFERENCES states(id) ON DELETE CASCADE,
+    state_id INT NOT NULL FOREIGN KEY (state_id) REFERENCES states(id),
     name VARCHAR(256)
 );
